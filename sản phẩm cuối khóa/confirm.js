72b8b82785f953af197b5ba5adcb0a19ac1a321b
vitoan_shop <nguyentoan281210@gmail.com>
2026-06-20 08:55:29 +0700

    update brand

--- sản phẩm cuối khóa/confirm.js
+++ sản phẩm cuối khóa/confirm.js
@@ -41,3 +41,25 @@ window.register = async function () {
 // } else {
 //     console.log("❌ Kết nối Firebase thất bại, hãy kiểm tra lại cấu hình.");
 // }
+
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCsazhg-XN02UcJbeJKhTS1vikgGiNckZk",
+//   authDomain: "spck2-822ad.firebaseapp.com",
+//   projectId: "spck2-822ad",
+//   storageBucket: "spck2-822ad.firebasestorage.app",
+//   messagingSenderId: "248899152311",
+//   appId: "1:248899152311:web:9b7426a9f89fdfe758a032",
+//   measurementId: "G-CGK6MBE6C2"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
