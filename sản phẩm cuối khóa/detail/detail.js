606f03f4e8b5273efccb725e29d1ecebd40531be
vitoan_shop <nguyentoan281210@gmail.com>
2026-06-30 22:20:09 +0700

    up 5

--- sản phẩm cuối khóa/detail/detail.js
+++ sản phẩm cuối khóa/detail/detail.js
@@ -1,40 +1,69 @@
-const products = [
-    {
-        id: 1,
-        name: "Áo thun in hình dáng rộng",
-        price: "350.000đ",
-        desc: "Áo thun cotton cao cấp, form rộng trẻ trung, mặc thoải mái.",
-        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQum7lX7otNboYJ_kRHe_8HHpvE9ggrR7CHyvJrHFagIsVnGffq5-SWtjp_TSEYi7vf6VSlmmRQF6X0qM_CsNDNlUsAsKwZ5XRcFk1spS5jT2xBiE-iswDBnBoZZK9D4fSfq_6XA64&usqp=CAc"
-    },
+// const products = [
+//     {
+//         id: 1,
+//         name: "Áo thun in hình dáng rộng",
+//         price: "350.000đ",
+//         desc: "Áo thun cotton cao cấp, form rộng trẻ trung, mặc thoải mái.",
+//         image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQum7lX7otNboYJ_kRHe_8HHpvE9ggrR7CHyvJrHFagIsVnGffq5-SWtjp_TSEYi7vf6VSlmmRQF6X0qM_CsNDNlUsAsKwZ5XRcFk1spS5jT2xBiE-iswDBnBoZZK9D4fSfq_6XA64&usqp=CAc"
+//     },
 
-    {
-        id: 2,
-        name: "Áo thun cotton in hình",
-        price: "320.000đ",
-        desc: "Áo thun cotton mềm mại, thấm hút mồ hôi tốt.",
-        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQYaT2bvfVuO9NC1IbiRkzI82RgHUIFhoQejxB83aeH_LFkvlXNL8KeAL6PgY8WJkp0xELdRY2ZLpek6XszXOKCBenst_wT5qKvlZPdEAjGi5aSVRDGxHV7UJrJ-koDall-rZTrTuk&usqp=CAc"
-    },
+//     {
+//         id: 2,
+//         name: "Áo thun cotton in hình",
+//         price: "320.000đ",
+//         desc: "Áo thun cotton mềm mại, thấm hút mồ hôi tốt.",
+//         image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQYaT2bvfVuO9NC1IbiRkzI82RgHUIFhoQejxB83aeH_LFkvlXNL8KeAL6PgY8WJkp0xELdRY2ZLpek6XszXOKCBenst_wT5qKvlZPdEAjGi5aSVRDGxHV7UJrJ-koDall-rZTrTuk&usqp=CAc"
+//     },
 
-    {
-        id: 3,
-        name: "Áo Thun Cổ Tròn Big Size Navy BS2763",
-        price: "450.000đ",
-        desc: "Áo big size phù hợp nhiều vóc dáng, chất liệu co giãn.",
-        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSiIa1UKt2JA6yTr0VSUKYxMFeT1XUNUGV9KeVJD38Yi8XT0MyDgZhjgSXYm_pe5VIKJCoHjvpWDObR-pTT60ZgZoHSHmIZj2uTDZyL2vPce9clzTt0eqjpWv6di2AmTWAf-8zIxwU&usqp=CAc"
-    }
+//     {
+//         id: 3,
+//         name: "Áo Thun Cổ Tròn Big Size Navy BS2763",
+//         price: "450.000đ",
+//         desc: "Áo big size phù hợp nhiều vóc dáng, chất liệu co giãn.",
+//         image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSiIa1UKt2JA6yTr0VSUKYxMFeT1XUNUGV9KeVJD38Yi8XT0MyDgZhjgSXYm_pe5VIKJCoHjvpWDObR-pTT60ZgZoHSHmIZj2uTDZyL2vPce9clzTt0eqjpWv6di2AmTWAf-8zIxwU&usqp=CAc"
+//     }
 
-    // Thêm các sản phẩm còn lại...
-];
+//     // Thêm các sản phẩm còn lại...
+// ];
 
-// Lấy id trên URL
+// // Lấy id trên URL
+// const params = new URLSearchParams(window.location.search);
+
+// const id = Number(params.get("id"));
+
+// const product = products.find(item => item.id === id);
+
+// if (product) {
+
+//     document.getElementById("product-img").src = product.image;
+
+//     document.getElementById("product-img").alt = product.name;
+
+//     document.getElementById("product-name").innerText = product.name;
+
+//     document.getElementById("product-desc").innerText = product.desc;
+
+//     document.getElementById("product-price").innerText = product.price;
+
+// } else {
+
+//     document.querySelector(".detail-container").innerHTML =
+//         "<h2>Không tìm thấy sản phẩm</h2>";
+
+// }
+
+import { products } from "../products.js";
+
 const params = new URLSearchParams(window.location.search);
+
 const id = Number(params.get("id"));
 
-// Tìm sản phẩm
 const product = products.find(item => item.id === id);
 
 if (product) {
+
     document.getElementById("product-img").src = product.image;
+
     document.getElementById("product-img").alt = product.name;
 
     document.getElementById("product-name").innerText = product.name;
@@ -42,8 +71,10 @@ if (product) {
     document.getElementById("product-desc").innerText = product.desc;
 
     document.getElementById("product-price").innerText = product.price;
+
 }
 else {
-    document.querySelector(".detail-container").innerHTML =
-        "<h2>Không tìm thấy sản phẩm!</h2>";
+
+    document.querySelector(".detail-container").innerHTML = "<h2>Không tìm thấy sản phẩm</h2>";
+
 }
---
+++ sản phẩm cuối khóa/product.js/product.js
@@ -0,0 +1,77 @@
+export const products = [
+
+    {
+        id: 1,
+        name: "Áo thun in hình dáng rộng",
+        price: "350.000đ",
+        desc: "Áo thun cotton cao cấp, form rộng trẻ trung.",
+        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQum7..."
+    },
+
+    {
+        id: 2,
+        name: "Áo thun cotton in hình",
+        price: "320.000đ",
+        desc: "Áo cotton mềm mại, thấm hút mồ hôi.",
+        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ..."
+    },
+
+    {
+        id: 3,
+        name: "Áo Thun Cổ Tròn Big Size Navy BS2763",
+        price: "450.000đ",
+        desc: "Áo big size phù hợp nhiều vóc dáng.",
+        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9Gc..."
+    },
+
+     {
+        id: 4,
+        name: "Áo Sơ Mi Tay Ngắn Nam Béo BS2969",
+        price: "450.000đ",
+        desc: "Áo big size phù hợp nhiều vóc dáng.",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSUhJ3Ry3SMQOegN3F-vlJXZRNeN6brTeuGelLmcpQikHON-yLh2wHJ_28o-7BfP6NahGLjEcJBjiT7EZo5t9ytwwIUaM4ksdI9apoT4GCdCSt71eWdZjDhKROzLNvbvOZflfBKatU&usqp=CAc"
+    },
+
+     {
+        id: 5,
+        name: "o Thun Khóa Kéo Size Từ M-8XL",
+        price: "450.000đ",
+        desc: " Chất Vải Cotton Dày Mềm Mịn Co Giãn Thương Hiệu Navy BS3274 – Màu Đen",
+        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRqEr23WCF8_vGk0lvedzFQeiXcec6EDi2zMJ5FCFKowPVWAY2zObtl8y0aoULp4o9IG9bO1qXE5-m0fNWRbvQ3DFwZ2ePnZZsJVBwZteXOJTGyE5fFO0QT2JnS1HO7WpGLjCDxEA&usqp=CAc"
+    },
+
+     {
+        id: 6,
+        name: "Áo Sơ Mi Tay Dài Nam Ngoại Cỡ Navy BS2973",
+        price: "450.000đ",
+        desc: "Áo big size phù hợp nhiều vóc dáng.",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQdP3EC0uC7q80J8lToIVJKPh4uUFE4WH-OPixLp_w2hYoHrM_2L9-uOck4L55pMK17nx5liSqhWF4c1SDVW_Y2W8DsMegY8TvB7iTcfnxoG775PxBTEFmgUCWRvPta1ylVmV3xLQ&usqp=CAc"
+    },
+
+     {
+        id: 7,
+        name: "Áo Sơ Mi Họa Tiết Đủ Size M-6XL",
+        price: "450.000đ",
+        desc: "Vải Sơ Mi Lụa Mềm Mỏng Mát Thương Hiệu Navy BS3319 – Màu Đen",
+        image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTh4Mkpfq4OQ8g1V0SEhB78KmzkhVchsVyNC5dWYXi0F1p6wkLyXCfzL4Ub01UzKuETCm2RSllu-u4l6gUixzuq6-iX3i87q2r-bc5u-O6BQiEHnLVNVqlRAw&usqp=CAc"
+    },
+
+     {
+        id: 8,
+        name: "ÁÁo thun in hình Loose Fit",
+        price: "450.000đ",
+        desc: "Áo big size phù hợp nhiều vóc dáng.",
+        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS9wK3zOwESbCKdTkj5kk0KrOOV8tJznqasQslY37-_BSvwvhPMYn0Ssxp1kgG4z0bpl4DGK7eEejUe5SzIrPh0nURtn06_uy6TDv0cM_h4g1y_XQi51whmBtiV4xaSfRe0k9hHB6hs&usqp=CAc"
+    },
+
+     {
+        id: 9,
+        name: "Áo Sơ Mi Tay Ngắn Nam Mập BS2952",
+        price: "450.000đ",
+        desc: "Áo big size phù hợp nhiều vóc dáng.",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS9MK5WvDKt6QnrSIJob-CFuKGj7m5UPRKcDyCl7vqldfH3TIiXMVHTFifZBBKqk29ZED71hxdGnE7yb7VDROB-VuD0nDol4lOi0I6kTjXqp8-k5BdPdWSnNPORAnj3ZiikkdJTvQ&usqp=CAc"
+    },
+
+
+
+];
